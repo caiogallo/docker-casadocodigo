0b24c4a8da8450b0e59f63937c64347b22a4fa00
Caio César Gallo <caio.gallo@gmail.com>
2015-11-24 20:43:27 +0000

    commit inicial

---
+++ README.md
@@ -0,0 +1,1 @@
+#Reposit√rio de testes do livro Constainers com Docker - Casa do C√digo
---
+++ cap3/docker_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DockerExample::Application.config.secret_token = '83fc7e1a18a9ec48c3571e3cebf1b194f2382228ab66e48eaaedfe050d75ee40b00919bd01287142ccb5928fdc7bd5f4d30e98d0d4fac2462659214fdc6db0f7'
